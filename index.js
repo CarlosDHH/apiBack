434194c696611686e3e0131f63784b09585d4de1
CarlosDHH <charlydhh18@gmail.com>
2024-03-19 21:29:35 -0600

    modified:   index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express=require('express')
-const mongoose=require('mongoose')
+const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors');
 
 require('dotenv').config()
 
-const app =express()
-const port= process.env.PORT||3000
+const app = express()
+const port = process.env.PORT || 3000
 
-const device=require('./src/routes/dispositivo')
-const product=require('./src/routes/productos')
-const typeUser=require('./src/routes/tipoUsuario')
-const user=require('./src/routes/usuarios')
+const device = require('./src/routes/dispositivo')
+const product = require('./src/routes/productos')
+const typeUser = require('./src/routes/tipoUsuario')
+const user = require('./src/routes/usuarios')
+const emp=require('./src/routes/empresa')
 
 
 //CORES
@@ -50,20 +51,21 @@ app.use(cors(corsOptions));
 //midlewares
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.json({"response":"Prueba de Device"})
+app.get('/', (req, res) => {
+    res.json({ "response": "Prueba de Device" })
 })
 
-app.use('/api/',device)
-app.use('/api/',product)
-app.use('/api/',typeUser)
-app.use('/api/',user)
+app.use('/api/', device)
+app.use('/api/', product)
+app.use('/api/', typeUser)
+app.use('/api/', user)
+app.use('/api/', emp)
 
 
 //coneccion con la base de dato
 mongoose.connect(process.env.mongouri)
-.then(()=>console.log('conectado a la base'))
-.catch(error=>console.log(error))
-app.listen(port,()=>{
-    console.log('corriendo en el puerto '+port)
+    .then(() => console.log('conectado a la base'))
+    .catch(error => console.log(error))
+app.listen(port, () => {
+    console.log('corriendo en el puerto ' + port)
 })
